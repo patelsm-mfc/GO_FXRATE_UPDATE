2ee2d69bbb1375c9ddfc467df870ffc77f005ef3
Smit <smit_patel@manulife.com>
2021-05-06 08:24:36 -0400

    removed old files

--- fxrates_processing.py
+++ fxrates_processing.py
@@ -3,12 +3,17 @@ from xml.dom import minidom
 import ntpath
 import sys
 
-currency_chk={  'AED' :False,'ANG' :False,'AUD' :False,'BBD' :False,'BMD' :False,'BND' :False,'BRL' :False,'BSD' :False,'BZD' :False,'CHF' :False,
-                'CNH' :False,'CNY' :False,'COP' :False,'CUP' :False,'DKK' :False,'DOP' :False,'EGP' :False,'EUR' :False,'GBP' :False,'GYD' :False,
-                'HKD' :False,'HTG' :False,'HUF' :False,'IDR' :False,'ILS' :False,'INR' :False,'JMD' :False,'JOD' :False,'JPY' :False,'KES' :False,
-                'KHR' :False,'KRW' :False,'KWD' :False,'KYD' :False,'LKR' :False,'MMK' :False,'MOP' :False,'MXN' :False,'MYR' :False,'NOK' :False,
-                'NZD' :False,'PAB' :False,'PHP' :False,'PLN' :False,'SEK' :False,'SGD' :False,'SRD' :False,'THB' :False,'TTD' :False,'TWD' :False,
-                'TZS' :False,'UGX' :False,'USD' :False,'UYU' :False,'VND' :False,'XCD' :False,'ZAR' :False,'ZMW' :False,'ZWD' :False    }
+#store currency list into boolean dict
+#store currency list into boolean dict
+currency_chk={}
+myfile = open("C:/Users/patelsm/Desktop/fx_rates/GO_FXRATE_UPDATE/currency_list.txt", "r")
+content = myfile.read()
+content_list = content.split(",")
+
+for line in content_list:    
+    currency_chk[line.strip()]=False
+myfile.close()
+
 
 def main():
     appended_data = []
@@ -20,21 +25,19 @@ def main():
     #process each file and append to final dataframe
     for x in range(len(lines)):
         appended_data.append(process(lines[x]))
-                
+
     appended_data = pd.concat(appended_data, ignore_index = True)
     #df = pd.DataFrame(list2)
-    appended_data.columns=['FinanceEnterpriseGroup','CurrencyTable','FromCurrency','ToCurrency','ExchangeDate','Rate']
+    #appended_data.columns=['FinanceEnterpriseGroup','CurrencyTable','FromCurrency','ToCurrency','ExchangeDate','Rate']
     print(appended_data)
     missingList = dict(filter(lambda elem: elem[1] == False, currency_chk.items()))
-    print(missingList.keys())   
-
-
-
+    print(missingList.keys()) 
 
 
 def process(file_name):
     tree = minidom.parse(file_name)
     spot_rate_type="BPL_CLASSIFIER_II"
+    quaterly_rate_type="ML_3M_AVG_PRIOR_RATE"
     seqnum=0
     items = tree.getElementsByTagName('Price')
     
@@ -46,11 +49,9 @@ def process(file_name):
             currency = item.getElementsByTagName("PRICE_CRNCY")[0].firstChild.nodeValue
             price_date = item.getElementsByTagName("ML_PRICE_DATE")[0].firstChild.nodeValue
             usd_to_rate = float(item.getElementsByTagName("ML_RATE")[0].firstChild.nodeValue)
-            temp_list = ["MFC","APCB","USD", currency, price_date, usd_to_rate]
-            list.append(temp_list)
-            usd_from_rate= float(1/usd_to_rate)       
-            temp_list = ["MFC","APCB",currency, "USD", price_date, usd_from_rate]
-            list.append(temp_list)
+            quarterly_rate=float(item.getElementsByTagName(quaterly_rate_type)[0].firstChild.nodeValue)           
+            temp_list = [currency, price_date, usd_to_rate,quarterly_rate]
+            list.append(temp_list)            
             currency_chk[currency]=True
     df = pd.DataFrame(list)
     
